1da03d64abe2f3e28e3cc935897c97901ce7cbf4
ShaksterNano <54268387+shaksternano@users.noreply.github.com>
2025-03-08 16:50:08 +0000

    fix: logo icon

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,6 @@
 import {FormEvent, useState} from "react";
 import {Link} from "react-router";
+import shelf from "../assets/shelf.svg";
 
 function Navbar() {
     const [isModalOpen, setIsModalOpen] = useState(false);
@@ -26,7 +27,7 @@ function Navbar() {
                 <div className="container mx-auto flex justify-between items-center">
                     <Link to="/">
                         <div className="flex items-center space-x-3">
-                            <img src="/src/assets/shelf.svg" alt="Logo" style={{height: "50px", width: "50px"}}/>
+                            <img src={shelf} alt="Logo" style={{height: "50px", width: "50px"}}/>
                             <span className="text-xl font-semibold text-white">
                                 Hoarders
                             </span>
